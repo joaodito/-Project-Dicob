848983c6ddfc795082e701743924934f11408d7f
raquel-andrade <107002526+raquel-andrade@users.noreply.github.com>
2022-09-01 19:52:39 -0300

    Update Expense.java

--- Dicob/src/br/com/dicob/model/Expense.java
+++ Dicob/src/br/com/dicob/model/Expense.java
@@ -2,4 +2,33 @@ package br.com.dicob.model;
 
 public class Expense {
 
+	//Attributes
+	private int idExpense;
+	private int dateExpense;
+
+	//Reference to this attributes
+	public Expense(int idExpense, int dateExpense){
+		this.idExpense = idExpense;
+		this.dateExpense = dateExpense;
+	}
+	
+	public Expense(){}
+
+	//Sets and gets of users 
+	public int getIdExpense() {
+		return idExpense;
+	}
+
+	public int getDateExpense() {
+		return dateExpense;
+	}
+
+	public void setDateExpense(int dateExpense) {
+		this.dateExpense = dateExpense;
+	}
+
+	//Constructor of a expense
+	public String toString() {
+		return idExpense + "" + dateExpense;
+	}
 }
