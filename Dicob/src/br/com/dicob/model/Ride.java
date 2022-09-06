466abf1f352af5e350553e5eec775195fa4721a7
raquel-andrade <107002526+raquel-andrade@users.noreply.github.com>
2022-09-05 21:14:20 -0300

    Update Ride.java

--- Dicob/src/br/com/dicob/model/Ride.java
+++ Dicob/src/br/com/dicob/model/Ride.java
@@ -1,12 +1,12 @@
 package model;
 
-public class Ride extends Expense {
-
+public class Ride extends Expense{
+	
 	//Attributes
 	private double quilometrosTrajeto;
 	private double precoCombustivel;
 	private int desempenhoPorQuilometro;
-	
+		
 	//Reference to this attributes
 	public Ride(double quilometrosTrajeto, double precoCombustivel, int desempenhoPorQuilometro) {
 		super(idExpense, nameExpense, dateExpense);
@@ -15,27 +15,51 @@ public class Ride extends Expense {
 		this.desempenhoPorQuilometro = desempenhoPorQuilometro;
 	}
 	
-	public Ride() {}
+	public Ride(){}
 	
 	//Sets and gets of ride
+	public int getIdExpense() {
+		return super.getIdExpense();
+	}
+	
+	public String getNameExpense() {
+		return super.getNameExpense();
+	}
+	
+	public int getDateExpense() {
+		return super.getDateExpense();
+	}
+
 	public double getQuilometrosTrajeto() {
 		return quilometrosTrajeto;
+	}	
+	
+	public double getPrecoCombustivel() {
+		return precoCombustivel;
+	}
+	
+	public int getDesempenhoPorQuilometro() {
+		return desempenhoPorQuilometro;
+	}
+	
+	public void setDateExpense(int dateExpense) {
+		super.setDateExpense(dateExpense);
+	}
+	
+	public void setNameExpense(String nameExpense) {
+		super.setNameExpense(nameExpense);
+	}
+	
+	public void setIdExpense(int idExpense) {
+		super.setIdExpense(idExpense);
 	}
 	
 	public void setQuilometroTrajeto(double quilometroTrajeto) {
 		this.quilometrosTrajeto = quilometroTrajeto;
 	}
 	
-	public double getPrecoCombustivel() {
-		return precoCombustivel;
-	}
-	
 	public void setPrecoCombustivel(double precoCombustivel) {
 		this.precoCombustivel = precoCombustivel;
-	}
-	
-	public int getDesempenhoPorQuilometro() {
-		return desempenhoPorQuilometro;
 	}
 	
 	public void setDesempenhoPorQuilometro(int desempenhoPorQuilometro) {
@@ -45,5 +69,6 @@ public class Ride extends Expense {
 	public String toString() {
 		return getIdExpense()+ ""+getNameExpense()+""+getDateExpense()+
 				quilometrosTrajeto+""+precoCombustivel+""+desempenhoPorQuilometro;
+		
 	}
 }
