486eae15590cfa5e600276cb077b0a049d08ced2
raquel-andrade <107002526+raquel-andrade@users.noreply.github.com>
2022-09-11 21:45:19 -0300

    Update TestUser.java

--- Dicob/src/br/com/dicob/test/TestUser.java
+++ Dicob/src/br/com/dicob/test/TestUser.java
@@ -1,5 +1,35 @@
-package br.com.dicob.test;
+package test;
 
-public class TestUser {
+import static org.junit.jupiter.api.Assertions.*;
 
-}
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import data.*;
+
+class TestUser {
+	private ControlData controlData;
+	@BeforeEach
+	void criarAtributo() {
+		controlData = new ControlData();
+	}
+	
+	@Test
+	void testRegisterUser() {
+		boolean test = controlData.registerUser("nome", "email", "cpf");
+		assertTrue(test);
+		assertTrue(controlData.getUsers().get(0).getNameUser().equals("nome"));
+		assertTrue(controlData.getUsers().get(0).getEmailUser().equals("email"));
+		assertTrue(controlData.getUsers().get(0).getCpfUser().equals("cpf"));	
+	}
+	
+	@Test
+	void testRegisterUserNull() {
+		boolean test = controlData.registerUser(null,"email", "cpf");
+		assertFalse(test);
+		boolean test2 = controlData.registerUser("nome", null, "cpf");
+		assertFalse(test2);
+		boolean test3 = controlData.registerUser("nome", "email", null);
+		assertFalse(test3);
+	}
+}	
