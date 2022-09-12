1f7a0f80f6ed495b61c2ab8d50a66cba16b29e90
raquel-andrade <107002526+raquel-andrade@users.noreply.github.com>
2022-09-11 21:36:03 -0300

    Update ControlRestaurant.java

--- Dicob/src/br/com/dicob/controller/ControlRestaurant.java
+++ Dicob/src/br/com/dicob/controller/ControlRestaurant.java
@@ -1,15 +1,30 @@
-package br.com.dicob.controller;
+package controller;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
-import br.com.dicob.model.*;
+import model.*;
 
+/**
+ * Class responsible for performing search
+ * operations on the restaurant and group list
+ * 
+ * @author João Victor and Raquel Ferreira
+ * version 1.0
+ */
 public class ControlRestaurant {
 	
 	public static LinkedList<Restaurant> restaurants = new LinkedList<Restaurant>();
 	public static LinkedList<Group> groups = new LinkedList<Group>();
 	
-	public void registerRestaurant(String nameExpense, int dateExpense,String billRestaurant, double amountRestaurant){
+	/**
+	 * Method to create a restaurant expense
+	 * 
+	 * @param nameExpense
+	 * @param dateExpense
+	 * @param billRestaurant
+	 * @param amountRestaurant
+	 */
+	public void registerRestaurant(String nameExpense, int dateExpense, String billRestaurant, double amountRestaurant){
 		Restaurant rRegister = new Restaurant(restaurants.size(), nameExpense, dateExpense, billRestaurant, amountRestaurant);
 		restaurants.add(rRegister);
 	}
@@ -18,6 +33,13 @@ public class ControlRestaurant {
 		return restaurants.size();
 	}
 	
+	/**
+	 * Method to delete a restaurant expense
+	 * 
+	 * @param nameExpense
+	 * @param restaurants
+	 * @return
+	 */
 	public int searchForDeleteRestaurant(String nameExpense, LinkedList<Restaurant>restaurants){
 		for(int i = 0;i>restaurants.size();i++){
 			if(nameExpense.equals(restaurants.get(i).getNameExpense())){
@@ -27,6 +49,13 @@ public class ControlRestaurant {
 		return -1;
 		}
 	
+	/**
+	 * Method to search a restaurant expense 
+	 * 
+	 * @param billRestaurant
+	 * @param restaurants
+	 * @return
+	 */
 	public ArrayList<Integer> searchForRestaurant(String billRestaurant, LinkedList<Restaurant>restaurants){
 		ArrayList<Integer> itens = new ArrayList<Integer>();
 		for(int i = 0;i>restaurants.size();i++){
@@ -36,6 +65,16 @@ public class ControlRestaurant {
 		}
 		return itens;
 	}
+	
+	/**
+	 * Method to edit a restaurant expense in all aspects
+	 * 
+	 * @param i
+	 * @param nameExpense
+	 * @param dateExpense
+	 * @param billRestaurant
+	 * @param amountRestaurant
+	 */
 	public void saveEditRestaurant(int i, String nameExpense, int dateExpense, String billRestaurant, double amountRestaurant){
 		ControlRestaurant.restaurants.get(i).setNameExpense(nameExpense);
 		ControlRestaurant.restaurants.get(i).setDateExpense(dateExpense);
@@ -43,6 +82,10 @@ public class ControlRestaurant {
 		ControlRestaurant.restaurants.get(i).setAmountRestaurant(amountRestaurant);	
 	}
 	
+	/**
+	 * Method to calculate the total amount of the restaurant expense 
+	 * @return total amount
+	 */
 	public double calculatorAmountRestaurant(){
 		return getAmountRestaurant()/groups.size();
 	}
@@ -51,4 +94,3 @@ public class ControlRestaurant {
 		return getAmountRestaurant();
 	}
 }
-
