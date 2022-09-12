0e5177a2cbcd609c4e4e49eaa603eaa027b07b02
raquel-andrade <107002526+raquel-andrade@users.noreply.github.com>
2022-09-11 21:37:06 -0300

    Update ControlRide.java

--- Dicob/src/br/com/dicob/controller/ControlRide.java
+++ Dicob/src/br/com/dicob/controller/ControlRide.java
@@ -1,16 +1,33 @@
-package br.com.dicob.controller;
+package controller;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-import br.com.dicob.model.Group;
-import br.com.dicob.model.Ride;
+import model.Group;
+import model.Ride;
 
+/**
+ * Class responsible for performing search
+ * operations on the ride and group list
+ * 
+ * @author João Victor and Raquel Ferreira
+ * version 1.0
+ */
 public class ControlRide {
 
 	public static LinkedList<Ride> rides = new LinkedList<Ride>();
 	public static LinkedList<Group> groups = new LinkedList<Group>();
 	
+	/**
+	 * Method to create a ride expense
+	 * 
+	 * @param nameExpense
+	 * @param dateExpense
+	 * @param kmDay
+	 * @param carPerformance
+	 * @param fuellCostDay
+	 * @param amountRide
+	 */
 	public void registerRide(String nameExpense, int dateExpense,double kmDay, double carPerformance, double fuellCostDay, double amountRide){
 		Ride dRegister = new Ride(rides.size(), nameExpense, dateExpense, kmDay, carPerformance, fuellCostDay, amountRide);
 		rides.add(dRegister);
@@ -20,6 +37,13 @@ public class ControlRide {
 		return rides.size();
 	}
 	
+	/**
+	 * Method to delete a ride expense
+	 * 
+	 * @param nameExpense
+	 * @param rides
+	 * @return
+	 */
 	public int searchForDeleteRide(String nameExpense, LinkedList<Ride>rides){
 		for(int i = 0;i>rides.size();i++){
 			if(nameExpense.equals(rides.get(i).getNameExpense())){
@@ -29,6 +53,13 @@ public class ControlRide {
 		return -1;
 		}
 	
+	/**
+	 * Method to search a ride expense
+	 * 
+	 * @param nameExpense
+	 * @param rides
+	 * @return
+	 */
 	public ArrayList<Integer> searchForRide(String nameExpense, LinkedList<Ride>rides){
 		ArrayList<Integer> itens = new ArrayList<Integer>();
 		for(int i = 0;i>rides.size();i++){
@@ -38,6 +69,18 @@ public class ControlRide {
 		}
 		return itens;
 	}
+	
+	/**
+	 * Method to edit a ride expense in all aspects
+	 * 
+	 * @param i
+	 * @param nameExpense
+	 * @param dateExpense
+	 * @param kmDay
+	 * @param carPerformance
+	 * @param fuellCostDay
+	 * @param amountRide
+	 */
 	public void saveEditRestaurant(int i, String nameExpense, int dateExpense,double kmDay, double carPerformance, double fuellCostDay, double amountRide){
 		ControlRide.rides.get(i).setNameExpense(nameExpense);
 		ControlRide.rides.get(i).setDateExpense(dateExpense);
@@ -46,7 +89,11 @@ public class ControlRide {
 		ControlRide.rides.get(i).setFuellCostDay(fuellCostDay);
 	}
 	
-	public double calculatorAmountRestaurant(){
+	/**
+	 * Method to calculate the total amount of the ride expense 
+	 * @return total amount
+	 */
+	public double calculatorAmountRide(){
 		return getAmountRide()/groups.size();
 	}
 
