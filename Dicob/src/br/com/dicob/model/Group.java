fa95ef0d92bb2ebc3e9a6770a0b9321e309d7d4c
raquel-andrade <107002526+raquel-andrade@users.noreply.github.com>
2022-09-11 21:42:31 -0300

    Update Group.java

--- Dicob/src/br/com/dicob/model/Group.java
+++ Dicob/src/br/com/dicob/model/Group.java
@@ -1,16 +1,31 @@
-package br.com.dicob.model;
+package model;
 
 import java.util.LinkedList;
 
+/**
+ * Class Group stores the information of a group
+ * 
+ * @author João Victor and Raquel Ferreira
+ * version 1.0
+ *
+ */
 public class Group {
 	
+	private static int idControl;
 	private int idGroup;
 	private String nameGroup;
 	private String categoriaGroup;
 	private LinkedList <User> groups;
 	
+	/**
+	 * Group Constructor
+	 * 
+	 * @param idGroup
+	 * @param nameGroup
+	 * @param categoriaGroup
+	 */
 	public Group(int idGroup, String nameGroup, String categoriaGroup){
-		this.idGroup = idGroup;
+		this.idGroup = idControl++;
 		this.nameGroup = nameGroup;
 		this.categoriaGroup = categoriaGroup;
 		this.groups = new LinkedList <User>();
@@ -37,10 +52,16 @@ public class Group {
 	public void setCategoriaGroup(String categoriaGroup) {
 		this.categoriaGroup = categoriaGroup;
 	}
-	
+
 	public LinkedList<User> getGroups() {
 		return this.groups;
 	}
+	
+	/**
+	 * Method to create group and add users
+	 * 
+	 * @param users
+	 */
 
 	public void registerGroup(User users){
 		this.groups.add(users);
