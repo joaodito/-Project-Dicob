2b91f294c75adb1470b95e39388b457924cd6ba5
raquel-andrade <107002526+raquel-andrade@users.noreply.github.com>
2022-09-11 21:38:36 -0300

    Update ControlData.java

--- Dicob/src/br/com/dicob/data/ControlData.java
+++ Dicob/src/br/com/dicob/data/ControlData.java
@@ -1,10 +1,15 @@
-package br.com.dicob.data;
+package data;
 
-import br.com.dicob.model.*;
-import java.util.List;
-import java.util.ArrayList;
+import model.*;
 import java.util.LinkedList;
 
+/** 
+ * Class ControlData mediates between the control and the
+ * view with CRUD operations
+ * 
+ * @author João Victor and Raquel Ferreira
+ * version 1.0
+ */
 public class ControlData {
 	private Data d = new Data();
 	
@@ -28,13 +33,19 @@ public class ControlData {
 		return this.d.getUsers();
 	}
 	
-	public LinkedList<Expense> getExpenses(){
-		return this.d.getExpenses();
+	public LinkedList<Restaurant> getRestaurants() {
+		return this.d.getRestaurants();
 	}
 	
-	public boolean controlObject(Object[] object){
-		for(Object o: object){
-			if(object == null){
+	/**
+	 * Method checks if the objects in the list are null, String or int
+	 * 
+	 * @param atr
+	 * @return true if an object is null, "" or 0, and false otherwise
+	 */
+	public boolean controlObject(Object[] atr){
+		for(Object o: atr){
+			if(atr == null){
 				return true;
 				}if(o.equals("")){
 					return true;
@@ -44,21 +55,36 @@ public class ControlData {
 						return false;
 						}
 					}
-				}	
-			
+		return false;
+	}	
 	
-	public boolean insertUser(String[] dataUser, String nameUser, String emailUser, int cpfUser){
-		Object[] atr = {nameUser, emailUser, cpfUser};
-		if (controlObject(atr)) return false;
-		User u = new User();
-		Data.uRegister(u);
+	/**
+	 * Method to create a user and add in users list
+	 * 		
+	 * @param nameUser
+	 * @param emailUser
+	 * @param cpf
+	 * @return true if succeed in create and add the user
+	 */
+	public boolean registerUser(String nameUser, String emailUser, String cpf){
+		
+		User uRegister = new User(nameUser, emailUser, cpf);
+		getUsers().add(uRegister);
 		return true;
 	}
 	
-	public boolean updateUser(String[] dataUser, int idUser, String nameUser, String emailUser, int cpfUser){
-		Object[] atr={};
-		if(controlObject(atr) || idUser < 0) return false;
-		User user = data.getUser(idUser);
+	/**
+	 * Method to edit a user in all aspects
+	 * 
+	 * @param idUser
+	 * @param nameUser
+	 * @param emailUser
+	 * @param cpfUser
+	 * @return true if succeed in edit the information of the user
+	 */
+	public boolean updateUser(int idUser, String nameUser, String emailUser, String cpfUser){
+		if(idUser < 0) return false;
+		User user = d.getUser(idUser);
 		if (user == null) return false;
 		user.setNameUser(nameUser);
 		user.setEmailUser(emailUser);
@@ -66,76 +92,126 @@ public class ControlData {
 		return true;
 	}
 	
+	/**
+	 * Method to delete a user from users list
+	 * 
+	 * @param idUser
+	 * @return 
+	 */
 	public boolean takeOutUser(int idUser){
 		if(idUser < 0){
 			return false;
 		}	
-		return data.deleteUser(IdUser);
+		return d.deleteUser(idUser);
 	}
 	
+	/**
+	 * Method to delete a group from group list
+	 * 
+	 * @param idGroup
+	 * @return
+	 */
 	public boolean takeOutGroup(int idGroup){
 		if(idGroup < 0){
 			return false;
 		}	
-		return data.deleteGroup(IdGroup);
+		return d.deleteGroup(idGroup);
 	}
 	
-	public boolean insertGroup(String[] dataGroup, String nameGroup, String categoryGroup){
+	/**
+	 * Method to create a group and add in group list
+	 * 
+	 * @param nameGroup
+	 * @param categoryGroup
+	 * @return true if succeed create and add the group
+	 */
+	public boolean registerGroup(String nameGroup, String categoryGroup){
 		Object[] atr = {nameGroup, categoryGroup};
 		if (controlObject(atr)) return false;
 		Group g = new Group();
-		Data.gRegister(g);
+		d.gRegister(g);
 		return true;
 	}
-	
-	public boolean updateGroup(String[] dataGroup, int idGroup, String nameGroup, String categoryGroup){
+	/**
+	 * Method to edit a group in all aspects
+	 * 
+	 * @param idGroup
+	 * @param nameGroup
+	 * @param categoryGroup
+	 * @return true if succeed edit the information of the group
+	 */
+	public boolean updateGroup(int idGroup, String nameGroup, String categoryGroup){
 		Object[] atr= {idGroup, nameGroup, categoryGroup};
 		if(controlObject(atr) || idGroup < 0) return false;
-		Restaurant group = data.getGroup(idGroup);
+		Group group = d.getGroup(idGroup);
 		if (group == null) return false;
 		group.setNameGroup(nameGroup);
-		group.setCategoryGroup(categoryGroup);
+		group.setCategoriaGroup(categoryGroup);
 		return true;
 	}
-	
-	public boolean insertRestaurant(String[] dataRestaurant, String nameExpense, int dateExpense, String billRestaurant, double amountRestaurant){
+	/**
+	 * Method to create a restaurant expense in a group
+	 * 
+	 * @param nameExpense
+	 * @param dateExpense
+	 * @param billRestaurant
+	 * @param amountRestaurant
+	 * @return true if succeed in create the restaurant expense, and false otherwise
+	 */
+	public boolean registerRestaurant(String nameExpense, int dateExpense, String billRestaurant, double amountRestaurant){
 		Object[] atr = {nameExpense, dateExpense, billRestaurant,amountRestaurant};
 		if (controlObject(atr)) return false;
 		Group g = new Group();
-		Data.gRegister(g);
+		d.gRegister(g);
 		return true;
 	}
 	
-	public boolean updateGroup(String[] dataGroup, int idGroup, String nameGroup, String categoryGroup){
-		Object[] atr= {idGroup, nameGroup, categoryGroup};
-		if(controlObject(atr) || idGroup < 0) return false;
-		Restaurant group = data.getGroup(idGroup);
-		if (group == null) return false;
-		group.setNameGroup(nameGroup);
-		group.setCategoryGroup(categoryGroup);
+	/**
+	 * Method to delete a restaurant expense
+	 * 
+	 * @param nameExpense
+	 * @param restaurants
+	 * @return
+	 */
+	public int searchForDeleteRestaurant(String nameExpense, LinkedList<Restaurant>restaurants){
+		for(int i = 0;i>restaurants.size();i++){
+			if(nameExpense.equals(restaurants.get(i).getNameExpense())){
+				return i;
+			}
+		}
+		return -1;
+		}
+	/**
+	 * Method to create a ride expense
+	 * 
+	 * @param nameExpense
+	 * @param dateExpense
+	 * @param kmDay
+	 * @param carPerformance
+	 * @param fuellCostDay
+	 * @param amountRide
+	 * @return true if succeed in create the ride expense
+	 */
+	public boolean registerRide(String nameExpense, int dateExpense, double kmDay, double carPerformance, double fuellCostDay, double amountRide){
+		Object[] atr = {nameExpense, dateExpense, kmDay, carPerformance, fuellCostDay, amountRide};
+		if (controlObject(atr)) return false;
+		Group g = new Group();
+		d.gRegister(g);
 		return true;
 	}
 	
-	public boolean takeOutExpenses(int idExpense){
-		LinkedList<Restaurant> restaurants = data.getRestaurants(idExpense);
-		for (Restaurant restaurant: restaurants){
-			for(User user: data.getUsers()){
-				user.delete(restaurant);
-			}
-			for(Group group: data.getGroups()){
-				group.delete(restaurant);
+	/**
+	 * Method to delete a ride expense
+	 * @param nameExpense
+	 * @param rides
+	 * @return
+	 */
+	public int searchForDeleteRide(String nameExpense, LinkedList<Ride>rides){
+		for(int i = 0;i>rides.size();i++){
+			if(nameExpense.equals(rides.get(i).getNameExpense())){
+				return i;
 			}
 		}
-		for (Ride ride: rides){
-			for(User user: data.getUsers()){
-				user.delete(ride);
-			}
-			for(Group group: data.getGroups()){
-				group.delete.(ride);
-			}
-			
+		return -1;
 		}
-		
-	}
-	
 }
